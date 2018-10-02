a6f56b45f60c294dbec5c79be3673e7631b70d1c
Matti Aho <ookooko@gmail.com>
2018-10-02 20:18:21 +0300

    3 days work. Some routes, fake database

--- server/server.js
+++ server/server.js
@@ -1,20 +1,52 @@
 const express = require('express');
+// var cors = require('cors')
+var bodyParser = require('body-parser')
+// app.use(cors());
 
 const database = {
   matches: [{
-
+    league: '',
+    round: 0,
+    homeTeam: '',
+    awayTeam: '',
+    homeGoals: 0,
+    awayGoals: 0,
+    homexG: 0,
+    awayxG: 0
   }]
 }
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('root');
 });
 
 app.get('/matches', (req, res) => {
-  res.send('matches');
+  res.json(database);
 });
+
+app.get('/match/:id', (req, res) => {
+  const id = req.params.id;
+  res.json(id);
+});
+
+app.post('/match', (req, res) => {
+  const {league, round, homeTeam, awayTeam, homeGoals, awayGoals, homexG, awayxG} = req.body;
+  database.matches.push({
+    league,
+    round,
+    homeTeam,
+    awayTeam,
+    homeGoals,
+    awayGoals,
+    homexG,
+    awayxG
+  })
+  console.log(database);
+  res.json(database.matches[database.matches.length - 1]);
+})
 
 app.listen(3001, () => {
   console.log('web server listening');
